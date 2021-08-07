39c3c3abefd8e38129ee4b673826ef2749a963fe
zz413012000 <zz413012000@gmail.com>
2021-08-07 10:01:41 +0800

    comicIndex

--- src/js/comicIndex-header.js
+++ src/js/comicIndex-header.js
@@ -21,7 +21,7 @@ class Header extends React.Component{
                     </div>
                     <div className="button">
                         <button>
-                            觀看第一話 &nbsp;>
+                            觀看第一話 &nbsp;&gt;
                         </button>
                     </div>
                 </div>
