6a0fc43d6914041d1c372b0937ecf99a947ef901
zz413012000 <zz413012000@gmail.com>
2021-08-18 21:49:37 +0800

    dangerouslySetInnerHTML

--- src/js/comicView-comment.js
+++ src/js/comicView-comment.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "../css/comicView-comment.css";
+let data="✨ 漫畫更新!!雞的顏值篇!! ✨\n💗鳥奴們總是能一眼辨認出自己的主子!💗\n鳥友敲碗的 「小雞評比篇01 」終於來啦！\n是說...文鳥到底要怎麼從外觀上分不同呢??\n\n之前看過有人可以一眼分出自己養的10隻蜥蜴，真是超厲害啊!🤩🤩";
+let str=data.replace(/(\r\n)|(\n)/g,"<br/>");
 function Comment(){
     return (
         <>
         <footer>
             <div className="creator">
                 <div className="creator_name">脆脆雞。作家</div>
-                <div className="creator_note">✨ 漫畫更新!!雞的顏值篇!! ✨<br/>
-                    💗鳥奴們總是能一眼辨認出自己的主子!💗<br/>
-                    鳥友敲碗的 「小雞評比篇01 」終於來啦！<br/>
-                    是說...文鳥到底要怎麼從外觀上分不同呢??<br/>
-                    之前看過有人可以一眼分出自己養的10隻蜥蜴，真是超厲害啊!🤩🤩
+                <div 
+                className="creator_note"
+                dangerouslySetInnerHTML={{__html: str}}>
                 </div>
             </div>
             <div className="comment">
