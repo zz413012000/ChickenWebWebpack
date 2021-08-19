ced8d534984bf06c981b33d5e26832e75f1d1891
zz413012000 <zz413012000@gmail.com>
2021-08-19 22:30:06 +0800

    View comment

--- src/js/comicView-comment.js
+++ src/js/comicView-comment.js
@@ -17,21 +17,29 @@ function Comment(){
                 <div className="comment_menu">
                     <div className="menu_top">
                         <div className="collect menuBtn_top">添加收藏</div>
-                        <div className="like menuBtn_top">喜歡</div>
+                        <div className="like menuBtn_top">
+                            喜歡
+                            <img srcSet="img/view/btn_like.png"></img>
+                        </div>
                         <div className="share menuBtn_top">分享</div>
                     </div>
                     <div className="menu_bottom">
                         <div className="like_btn menuBtn_bottom">
-                            <div className="like_icon"></div>
+                            <div className="like_icon">
+                                <img srcSet="img/view/yellowHeart.png"></img>
+                            </div>
                             <div className="like_number">248</div>
                         </div>
                         <div className="comment_btn menuBtn_bottom">
-                            <div className="comment-icon"></div>
+                            <div className="comment-icon">
+                                <img srcSet="img/view/comment.png"></img>
+                            </div>
                             <div className="comment_number">21</div>
                         </div>
                     </div>
                 </div>
                 <div className="comment_area">
+                    
                     <div className="commentBox">
                         <div className="comment_head">
                             <div className="userName">小雞特攻隊</div>
@@ -39,26 +47,33 @@ function Comment(){
                             <div className="commentReport">檢舉</div>
                         </div>
                         <div className="comment_container">
-                            <div className="icon_best">best!</div>
+                            <div className="icon_best">
+                                <img srcSet="img/view/best.png"></img>
+                            </div>
                             <div className="comment_contents">真是無法無天的雞仔!真是無法無天的雞仔!真是無法無天的雞仔!真是無法無天的雞仔!真是無法無天的雞仔!</div>
                         </div>
-                        <div className="comment_lick">
+                        <div className="comment_like">
                             讚 30
                         </div>
                     </div>
-                    <div className="commentBox">
+                    <div className="commentBox">      
                         <div className="comment_head">
                             <div className="userName">小雞特攻隊</div>
                             <div className="commentTime">2022年8月1日</div>
                             <div className="commentReport">檢舉</div>
                         </div>
                         <div className="comment_container">
-                            <div className="icon_best">best!</div>
+                            <div className="icon_best">
+                                <img srcSet="img/view/best.png"></img>
+                            </div>
                             <div className="comment_contents">真是無法無天的雞仔!真是無法無天的雞仔!真是無法無天的雞仔!真是無法無天的雞仔!真是無法無天的雞仔!</div>
                         </div>
-                        <div className="comment_lick">
+                        <div className="comment_like">
                             讚 30
                         </div>
+                    </div>
+                    <div className="comment_cloud">
+                        <img srcSet="img/view/bottomCloud.png"></img>
                     </div>
                 </div>
             </div>
