2310cb899845035c19e625f2093b253fbf48fde6
zz413012000 <zz413012000@gmail.com>
2021-09-17 05:50:37 +0800

    Delete log

--- src/js/comicView-comment.js
+++ src/js/comicView-comment.js
@@ -3,11 +3,7 @@ import "../css/comicView-comment.css";
 let data="✨ 漫畫更新!!雞的顏值篇!! ✨\n💗鳥奴們總是能一眼辨認出自己的主子!💗\n鳥友敲碗的 「小雞評比篇01 」終於來啦！\n是說...文鳥到底要怎麼從外觀上分不同呢??\n\n之前看過有人可以一眼分出自己養的10隻蜥蜴，真是超厲害啊!🤩🤩";
 let str=data.replace(/(\r\n)|(\n)/g,"<br/>");
 function Comment(props){
-    // let src=location.href.split("?")[0];
-    // let src=location.origin+location.pathname;
     let src=location.href;
-    console.log(src);
-    // console.log(location.href);
     let src2="https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(src);
     return (
         <>
